7a08476a35e5ff8021ba320bf42d79765d1b710f
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-04-01 20:57:04 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/main/java/com/example/udapagent1/Udapagent1Application.java
@@ -0,0 +1,13 @@
+package com.example.udapagent1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Udapagent1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Udapagent1Application.class, args);
+	}
+
+}
